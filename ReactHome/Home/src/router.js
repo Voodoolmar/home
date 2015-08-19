f7453f614dbe806911fc0e03249c8c4e7fe3aae5
p.beltyukov <poll-1@mail.ru>
2015-08-19 18:06:30 +0600

    Initial

---
+++ ReactHome/Home/src/router.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { Route } from 'react-router';
+import App from './components/App';
+import ContentPage from './components/ContentPage';
+
+var routes = (
+	  <Route path="/" handler={App}>
+		  <Route path="privacy" handler={ContentPage}/>
+	  </Route>
+	);
+export default routes;

417de850014e1473c2659482f03f1c298d307290
Poll <poll-1@mail.ru>
2016-02-27 16:21:23 +0600

    1

--- RO/RO/app.js
+++ RO/RO/app.js
@@ -7,19 +7,28 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var lessMiddleware = require('less-middleware');
 
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-//app.use(express.logger('dev'));
+app.use(express.favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(lessMiddleware('/less',
+	{
+        dest: '/css',
+        pathRoot: path.join(__dirname, 'public'), 
+        debug: true
+	}
+));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only

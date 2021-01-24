699c9990c17482b57245a55883b6ed8a2cf5eee9
voodoolmar <voodoolmar@hotmail.com>
2021-01-24 20:46:44 +0700

    1

---
+++ ProMamaClub/ProMamaClub/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace ProMamaClub
+{
+	public class Constants
+	{
+		public const string SiteName = "ProMamaClub.ru";
+		public const string Name = "Мама клуб skdjhfskjd kjsdhf ksjd kshdf";
+    }
+}
---
+++ ProMamaClub/publish.bat
@@ -0,0 +1,2 @@
+docker tag promamaclub:latest voodoolmar/promamaclub:latest
+docker push voodoolmar/promamaclub:latest
